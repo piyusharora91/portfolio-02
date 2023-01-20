c1670df426390d434c02505d975131cb0bbbde78
piyusharora91 <piyusharora596@gmail.com>
2023-01-20 17:00:50 +0530

    Parallax effects update, JS File Structure update

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,10 @@
-$(document).ready(function () {
-
-    $('#menu').click(function () {
+$(window).load(function () {
+    $('#menu').on('click', function () {
         $(this).toggleClass('fa-times');
         $('header').toggleClass('toggle');
     });
 
-    $(window).on('scroll load', function () {
-
-        $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle');
-
-        if ($(window).scrollTop() > 0) {
-            $('.top').show();
-        } else {
-            $('.top').hide();
-        }
-    });
-
-    $('#mail').click(function () {
+    $('#mail').on('click', function () {
         const mail_text = $('#mail span').text();
         if (mail_text) {
             navigator.clipboard.writeText(mail_text).then(() => {
