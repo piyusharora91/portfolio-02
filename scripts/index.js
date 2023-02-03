701915b9417891ddd8b74bc9bca60cc6acd612d0
Piyush <piyusharora596@gmail.com>
2023-02-03 18:38:44 +0530

    nav link animation added, with updated transition timeing for header

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,20 @@
 const hambergerIcon = document.getElementById("menu");
 const mailValue = document.getElementById("mail-value");
 const header = document.querySelector("header");
+const navLinks = document.querySelectorAll(".nav-links");
 
 document.addEventListener("DOMContentLoaded", () => {
-  //toggle header in mobile/tablet browsers
+  //toggle header in mobile/tablet browsers with hamberger click
   hambergerIcon.addEventListener("click", () => {
     hambergerIcon.classList.toggle("fa-times");
     header.classList.toggle("toggle");
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      header.classList.remove("toggle");
+      hambergerIcon.classList.remove("fa-times");
+    });
   });
 
   //copy mail functioanlity
