99b744766d093658de1ad66cde87d052804fd7e1
Piyush <piyusharora596@gmail.com>
2023-02-03 12:21:45 +0530

    removed jquery and added vanilla JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,41 @@
-$(window).load(function () {
-    $('#menu').on('click', function () {
-        $(this).toggleClass('fa-times');
-        $('header').toggleClass('toggle');
-    });
+const hambergerIcon = document.getElementById("menu");
+const mailValue = document.getElementById("mail-value");
+const header = document.querySelector("header");
 
-    $('#mail').on('click', function () {
-        const mail_text = $('#mail span').text();
-        if (mail_text) {
-            navigator.clipboard.writeText(mail_text).then(() => {
-                alert('Mail Copied');
-            })
-                .catch(() => {
-                    alert('Failed To Copy Mail To Clipboard');
-                });
-        }
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  //   $("#menu").on("click", function () {
+  //     $(this).toggleClass("fa-times");
+  //     $("header").toggleClass("toggle");
+  //   });
+
+  hambergerIcon.addEventListener("click", () => {
+    hambergerIcon.classList.toggle("fa-times");
+    header.classList.toggle("toggle");
+  });
+
+  //   $("#mail").on("click", function () {
+  //     const mail_text = $("#mail span").text();
+  //     if (mail_text) {
+  //       navigator.clipboard
+  //         .writeText(mail_text)
+  //         .then(() => {
+  //           alert("Mail Copied");
+  //         })
+  //         .catch(() => {
+  //           alert("Failed To Copy Mail To Clipboard");
+  //         });
+  //     }
+  //   });
+  mailValue.addEventListener("click", () => {
+    if (mailValue.textContent) {
+      navigator.clipboard
+        .writeText(mailValue.textContent)
+        .then(() => {
+          alert("Mail Copied");
+        })
+        .catch(() => {
+          alert("Failed To Copy Mail To Clipboard");
+        });
+    }
+  });
 });
